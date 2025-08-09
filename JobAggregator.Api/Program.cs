947c1af4e27459692abf35cdada14c63ac163300
Akash Jatwa <55755027+akashjatwa@users.noreply.github.com>
2025-08-09 17:46:59 +0530

    Resolve merge conflicts with main

--- JobAggregator.Api/Program.cs
+++ JobAggregator.Api/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
-using JobAggregator.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using JobAggregator.Infrastructure.Data;
 using Serilog;
 using Serilog.Formatting.Json;
 
