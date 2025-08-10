5922d385fcab53bf37373d5eeb03aab130cfc145
Akash Jatwa <akashjatwa85@gmail.com>
2025-08-10 22:53:23 +0530

    Update HealthController.cs

--- JobAggregator.Api/Controllers/HealthController.cs
+++ JobAggregator.Api/Controllers/HealthController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading.Tasks;
 
 namespace JobAggregator.Api.Controllers;
 
